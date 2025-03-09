9561be5215ce79406c409aea116eadbaec53e7ee
Renan Rocha <renanro919@gmail.com>
2025-03-09 11:38:02 -0400

    adiciona homepage ao package.json e altera o roteador para HashRouter no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import ListaTreinos from "./pages/listaTreinos";
 
